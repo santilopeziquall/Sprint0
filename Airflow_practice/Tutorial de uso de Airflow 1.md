3f8604d003a0438c3d3b189521b9cddc482d8add
Sincrho <97929350+santiagolopezdev@users.noreply.github.com>
2022-05-24 16:56:48 -0300

    Unidad 4 y Apache Airflow
    Completo los prácticos de la unidad 4 y el tutorial de uso 1

---
+++ Airflow_practice/Tutorial de uso de Airflow 1.md
@@ -0,0 +1,65 @@
+## Tutorial de uso de Airflow 1
+
+### Cambios que tuve que realizar
+
+##### Durante la instalacion
+
+- Instalar virtualenv (en mi caso no lo tenia)
+  `pip install virtualenv`
+
+- El comando de instalacion del tutorial 
+  ```pip install airflow == 1.8.0  ```
+  esta deprecado usar: 
+  `pip install apache-airflow` 
+
+##### Para inicializar la db
+
+- El comando `airflow initdb` esta deprecado usar: `airflow db init`
+
+##### Cuando se quiere hacer import de un operator propio
+
+En el tutorial te indican que hagas el import de manera
+
+```python
+from airflow.operators import MyFirstOperator
+```
+
+A partir de la version 2.0 de Apache Airflow los imports se tienen que hacer de la siguiente manera:
+
+```python
+from my_operators import MyFirstOperator
+```
+
+Donde la estructura de archivos se ve de esta forma:
+
+<img src="./Images/Imagen1.png" title="" alt="" width="215">
+
+##### Cuando se quiere testear una tarea dentro de un DAG
+
+En el tutorial dice de usar
+
+```bash
+airflow test my_test_dag my_first_operator_task 2017-03-18T18:00:00.0`
+```
+
+La manera que se usa ahora es:
+
+```bash
+airflow tasks test my_test_dag my_first_operator_task 2022-05-24
+```
+
+##### Cuando se quiere importar BaseSensorOperator
+
+En el bloque del tutorial se usa:
+
+```python
+from airflow.operators.sensors import BaseSensorOperator
+```
+
+La manera que se usar ahora es:
+
+```python
+from airflow.sensors.base import BaseSensorOperator
+```
+
+
---
+++ Airflow_practice/airflow_home/plugins/my_operators.py
@@ -0,0 +1,46 @@
+import logging
+
+from airflow.models import BaseOperator
+from airflow.plugins_manager import AirflowPlugin
+from airflow.utils.decorators import apply_defaults
+from datetime import datetime
+from airflow.sensors.base import BaseSensorOperator
+
+log = logging.getLogger(__name__)
+
+class MyFirstOperator(BaseOperator):
+
+    @apply_defaults
+    def __init__(self, my_operator_param, *args, **kwargs):
+        self.operator_param = my_operator_param
+        super(MyFirstOperator, self).__init__(*args, **kwargs)
+
+    def execute(self, context):
+        log.info("Hello World!")
+        #from IPython import embed; embed()
+        log.info('operator_param: %s', self.operator_param)
+        task_instance = context['task_instance']
+        sensors_minute = task_instance.xcom_pull('my_sensor_task', key='sensors_minute')
+        log.info('Valid minute as determined by sensor: %s', sensors_minute)
+
+
+class MyFirstSensor(BaseSensorOperator):
+
+    @apply_defaults
+    def __init__(self, *args, **kwargs):
+        super(MyFirstSensor, self).__init__(*args, **kwargs)
+
+    def poke(self, context):
+        current_minute = datetime.now().minute
+        if current_minute % 3 != 0:
+            log.info("Current minute (%s) not is divisible by 3, sensor will retry.", current_minute)
+            return False
+
+        log.info("Current minute (%s) is divisible by 3, sensor finishing.", current_minute)
+        task_instance = context['task_instance']
+        task_instance.xcom_push('sensors_minute', current_minute)        
+        return True
+
+class MyFirstPlugin(AirflowPlugin):
+    name = "my_first_plugin"
+    operators = [MyFirstOperator,MyFirstSensor]
